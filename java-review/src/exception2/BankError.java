1c58da3b46068ef0bbc30c7007594e330fad1c14
yhayeong <staccatissimo123@gmail.com>
2023-08-28 19:12:30 +0900

    0828

---
+++ java-review/src/exception2/BankError.java
@@ -0,0 +1,10 @@
+package exception2;
+
+public enum BankError {
+	NOID, EXISTID, LACK, MINUS, MENU, NOTAGRADE
+	//계좌번호오류, 중복계좌번호, 잔액부족, 입금액/출금액입력오류, 메뉴오류
+	//연관된 상수들의 집합으로, 초기화가능한 값을 정해둠
+	
+//	public static final BankError NOID = new BankError();
+
+}
